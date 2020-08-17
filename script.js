56ab2daefadef6ba615c64c30921768fbc803756
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2020-08-17 13:46:58 +0930

    Basic Strings

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstName= 'Jhon';
+let lastName='Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year){
+    return 2020-yearOfBirth;
+}
+
+
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth} and now he is ${calcAge(yearOfBirth)} years old.`);
